d3fa409f5ab617f3df1edc343ddaea59293b803e
Данила Ерофеев <98770710+E7D1A8@users.noreply.github.com>
2023-11-22 13:13:44 +0300

    Yandex_api_Main_Kits

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://84f34fff-abff-4e13-bfa9-5d18745a275b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+CREATE_KITS_PATH = "/api/v1/kits/"
---
+++ data.py
@@ -0,0 +1,20 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
+
+created_kit = {
+       "name": "Мой набор",
+       "card": {
+           "id": 1,
+           "name": "Под ситуацию"
+       },
+       "productsList": "null",
+       "id": 7,
+       "productsCount": 0
+}
