8a7ff91ea853226a1691fe167088e1cbf63deb45
Danila Erofeev <veefored@yandex.ru>
2023-11-22 15:25:25 +0300

    Yandex_api_Main_Kits

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 headers = {
     "Content-Type": "application/json"
-
 }
 
 user_body = {
@@ -8,3 +7,17 @@ user_body = {
     "phone": "+79995553322",
     "address": "г. Москва, ул. Пушкина, д. 10"
 }
+
+headers = {
+    "Content-Type": "application/json"
+}
+created_kits = {
+       "name": "Мой набор",
+       "card": {
+           "id": 1,
+           "name": "Под ситуацию"
+       },
+       "productsList": "null",
+       "id": 7,
+       "productsCount": 0
+   }
