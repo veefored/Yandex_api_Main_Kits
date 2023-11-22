2ca91622639f820944ea96f66aeb9c13bad79f12
Danila Erofeev <veefored@yandex.ru>
2023-11-22 16:21:56 +0300

    Yandex_api_Main_Kits

--- data.py
+++ data.py
@@ -12,13 +12,13 @@ headers = {
     "Content-Type": "application/json"
     "Authorization" "Bearer 8ffc5787-1dd5-4323-aa5d-52e0abe0b7af"
 }
-create_kits = {
+Create_kits = {
        "name": "Мой набор",
        "card": {
            "id": 1,
            "name": "Под ситуацию"
        },
-    "productsList": "null",
-    "id": 7,
-    "productsCount": 0
+       "productsList": "null",
+       "id": 7,
+       "productsCount": 0
    }
