ae7d8aad3675e799e5c980d08cf082ff68cfbf59
Danila Erofeev <veefored@yandex.ru>
2023-11-22 15:01:56 +0300

    Yandex_api_Main_Kits

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,11 +5,5 @@ def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
                          json=body,  # тут тело
                          headers=data.headers)  # а здесь заголовки
-def post_new_kits(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
-                         json=body,
-                         headers=data.headers)
-str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
-               + user_body["address"] + ",,," + user_response.json()["authToken"]
-response = post_new_user(data.user_body);
+
 print(response.status_code)
