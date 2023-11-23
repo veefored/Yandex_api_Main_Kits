dfd6c3ae83f3a59853b7ef902a447de231698bf0
Danila Erofeev <veefored@yandex.ru>
2023-11-23 18:20:28 +0300

    Yandex_api_Main_Kits

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://f8ee1cc2-8177-41d9-a1ee-99bafea391ab.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://74ec1036-25ca-459b-a6c2-e3221c27a615.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KITS_PATH = "/api/v1/kits/"
