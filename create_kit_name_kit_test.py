6c3d2d16e39d251e0780a5ff5d2d03306116ba6a
Danila Erofeev <veefored@yandex.ru>
2023-11-23 15:09:49 +0300

    Yandex_api_Main_Kits

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -2,7 +2,14 @@ import sender_stand_request
 import data
 def post_new_kit(name):
     current_kit_name = data.kit_body.copy()
-    current_kit_name["firstName"] = first_name
+    current_kit_name["firstName"] = name
+
     return current_kit_name
+def positive_assert(name):
+    kit_body = post_new_kit(name)
+    kit_response = sender_stand_request.post_new_kit()
+
+    assert kit_response.status_code == 201
+    assert kit_response.json()["authToken"] != ""
 def test_create_user_2_letter_in_first_name_get_success_response():
     positive_assert("Aa")
