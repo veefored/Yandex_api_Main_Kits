fe83df696eb9e7466d45909398e604841197baab
Danila Erofeev <veefored@yandex.ru>
2023-11-23 15:01:29 +0300

    Yandex_api_Main_Kits

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,12 +1,8 @@
 import sender_stand_request
-
-
-# Проверяется, что код ответа равен 201
-assert user_response.status_code == 201
-# Проверяется, что в ответе есть поле authToken, и оно не пустое
-assert user_response.json()["authToken"] != ""
-
-
-
-data.authToken ["Authorization"] = response.json()
-print (data.authToken)
+import data
+def post_new_kit(name):
+    current_kit_name = data.kit_body.copy()
+    current_kit_name["firstName"] = first_name
+    return current_kit_name
+def test_create_user_2_letter_in_first_name_get_success_response():
+    positive_assert("Aa")
