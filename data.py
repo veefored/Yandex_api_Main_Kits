7185953b927798013c4d00e69891c23385d63059
Danila Erofeev <veefored@yandex.ru>
2023-11-23 12:09:50 +0300

    Yandex_api_Main_Kits

--- data.py
+++ data.py
@@ -6,10 +6,10 @@ user_body = {
     "phone": "+79995553322",
     "address": "г. Москва, ул. Пушкина, д. 10"
 }
-headers_authtoken = {
+headers_authToken = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer {0e4d75c5-531e-4cd7-9c23-81bc9ddf135f}"
+    "Authorization": "Bearer f92dc3ae-90cc-49cb-b7b6-fd0031570775"
 }
 kit_body = {
-       "name": "New_kit"
+       "name": "Мой набор"
 }
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,10 +1,23 @@
 import configuration
 import requests
 import data
-def post_new_user(body):
+def post_new_user (body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
                          json=body,
                          headers=data.headers)
-response = post_new_user(data.user_body);
-print(response.status_code)
+
+response = post_new_user (data.user_body);
+
+print (response.status_code)
+print (response.json())
+
+data.headers_authToken ["Authorization"] = response.json()
+print (data.headers_authToken)
+def post_new_kit (body):
+    return requests.post (configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
+                         json=body,
+                         headers=data.headers)
+
+response = post_new_kit(data.kit_body)
+print (response.status_code)
 print (response.json())
