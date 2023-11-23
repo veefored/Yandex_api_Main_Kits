120ead50bedc4654a5484b2a34f55a7742f95775
Danila Erofeev <veefored@yandex.ru>
2023-11-23 13:47:17 +0300

    Yandex_api_Main_Kits

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://611772df-a999-48c0-82e3-5ae4daad90ea.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://f8ee1cc2-8177-41d9-a1ee-99bafea391ab.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KITS_PATH = "/api/v1/kits/"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -10,15 +10,20 @@ response = post_new_user (data.user_body);
 
 print (response.status_code)
 print (response.json())
-def get_new_user_authToken():
-    return requests.get(data.authToken, json=data.authToken)
-data.get_new_user_authToken["Autorization"]
+def get_new_user_authToken(body):
+    return requests.get(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                        json=authToken)
+
+response = data.authToken ["Authorization"]
+
 print (data.authToken)
 def post_new_client_kit (body):
     return requests.post (configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
                          json=data.kit_body,
-                         headers=data.headers)
+                         headers=data.headers,
+                         params=data.kit_body)
 
 response = post_new_client_kit(data.kit_body)
+
 print (response.status_code)
 print (response.json())
