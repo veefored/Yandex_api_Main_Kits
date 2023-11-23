ebbf8f92aa7c3fd5d27747016d044dcf7c0776df
Danila Erofeev <veefored@yandex.ru>
2023-11-23 20:15:41 +0300

    Yandex_api_Main_Kits

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,18 +1,16 @@
 import sender_stand_request
 import data
 
-
-# эта функция меняет значения в параметре firstName
-def get_new_kit_body(name):
+def post_new_kit_body(name):
     current_kit_body = data.kit_body.copy()
     current_kit_body["Name"] = name
 
     return current_kit_body
 
-
 # Тест 1. Параметр kit_Name состоит из 1 символа
-def test_create_kit_name_1_letter_in_name_get_success_response():
-    kit_body = get_new_kit_body("a")
-    kit_response = sender_stand_request.kit(kit_body)
+def test_create_new_kit_name_1_letter_in_name_get_success_response():
+    kit_body = post_new_kit_body("a")
+    kit_response = sender_stand_request.post.new_kit_name(kit_body)
+
     assert kit_response.status_code == 201
     assert kit_response.json()["name"] == kit_body["name"]
