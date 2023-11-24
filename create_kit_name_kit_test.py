a9e5834e1273cb71efaa8646e48cd5e2e104af47
Danila Erofeev <veefored@yandex.ru>
2023-11-24 10:37:29 +0300

    Yandex_api_Main_Kits

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -29,3 +29,12 @@ def test_create_new_kit_name_511_letter_in_name_post_success_response():
     assert kit_response.status_code == 201
     assert kit_response.json()["name"] == kit_body["name"]
 
+# Тест 3. Параметр kit_Name состоит из Количество символов меньше допустимого (0)
+
+def test_create_new_kit_name_0_letter_in_name_post_success_response():
+    kit_body = post_new_kit_body("")
+    kit_response = sender_stand_request.post_new_client_kit("")
+
+    assert kit_response.status_code == 400
+    assert kit_response.json()["name"] == kit_body["name"]
+
